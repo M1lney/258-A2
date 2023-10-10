56f6b91e998944b914040f558bf6d90adb9af111
Tom Milne <tomj.milne@gmail.com>
2023-10-10 17:18:54 +1300

    first commit

---
+++ 258-A2-Tom-Milne/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace _258_A2_Tom_Milne.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
